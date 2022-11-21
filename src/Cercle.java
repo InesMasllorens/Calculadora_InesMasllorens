1e4f77b93aa28f037b565a24844f51b872370973
Ines Masllorens <ines.maria.masllorens@estudiantat.upc.edu>
2022-11-21 20:27:12 +0100

    polimorfisme Ines Masllorens

--- src/Cercle.java
+++ src/Cercle.java
@@ -1,9 +1,11 @@
 public class Cercle extends Figura{
     private double r;
+    private double r;
     public Circulo(double radio){
         this.r=radio;
     }
     public double area(){
+
         return (Math.PI*this.r*this.r);
     }
 }
